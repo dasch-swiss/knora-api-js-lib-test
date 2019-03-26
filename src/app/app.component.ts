2c40f6f2c14ea3f4577d28ad2b2f534f58a5ece7
Andreas Aeschlimann <andreas.aeschlimann@appvision.ch>
2019-03-26 18:39:32 +0100

    Added button to gui

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,17 +9,21 @@ import { KnoraApiConfig, KnoraApiConnection } from "@knora/api";
 export class AppComponent implements OnInit {
 
     ngOnInit() {
+    }
+
+    doRequest() {
 
         const config = new KnoraApiConfig("http", "localhost", 3333);
         const knoraApiConnection = new KnoraApiConnection(config);
 
         console.log(knoraApiConnection);
 
+        //knoraApiConnection.v2.auth.login();
+
         knoraApiConnection.admin.users.getAll().subscribe(
             a => console.log(a),
             b => console.error(b)
         );
-
     }
 
 }
