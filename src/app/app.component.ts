765005a5ab9c15a730f8fe6d01f8e73f97a58238
Andreas Aeschlimann <andreas.aeschlimann@appvision.ch>
2019-04-01 19:03:50 +0200

    Added simple login and logout test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,22 +8,43 @@ import { KnoraApiConfig, KnoraApiConnection } from "@knora/api";
 })
 export class AppComponent implements OnInit {
 
+    knoraApiConnection: KnoraApiConnection;
+
     ngOnInit() {
+        const config = new KnoraApiConfig("http", "localhost", 3333);
+        this.knoraApiConnection = new KnoraApiConnection(config);
+        console.log(this.knoraApiConnection);
     }
 
-    doRequest() {
+    login() {
 
-        const config = new KnoraApiConfig("http", "localhost", 3333);
-        const knoraApiConnection = new KnoraApiConnection(config);
+        this.knoraApiConnection.v2.auth.login("root", "test").subscribe(
+            (token: string) => {
+                console.log(token);
+            },
+            error => console.error(error)
+        );
 
-        console.log(knoraApiConnection);
+        /**/
 
-        //knoraApiConnection.v2.auth.login();
+    }
 
-        knoraApiConnection.admin.users.getAll().subscribe(
+    logout() {
+
+        this.knoraApiConnection.v2.auth.logout().subscribe(
             a => console.log(a),
             b => console.error(b)
         );
+
+    }
+
+    test() {
+
+        this.knoraApiConnection.admin.users.getAll().subscribe(
+            a => console.log(a),
+            b => console.error(b)
+        );
+
     }
 
 }
